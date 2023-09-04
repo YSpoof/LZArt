3dadd8c1735756c73b4ac8c989af9aa4427c21c9
YSpoof <32226366+YSpoof@users.noreply.github.com>
2023-09-04 14:10:22 -0300

    Initial Commit

---
+++ src/siteData.ts
@@ -0,0 +1,40 @@
+export default {
+  siteName: "LZArt",
+  siteDescription: "Be simple",
+  contatEmail: "contato@lzart.com.br",
+  siteLogo: "/logo.webp",
+  bgImage: "/bg.webp",
+  projects: [
+    {
+      title: "Tarefa Educação",
+      url: "https://tarefaedu.lzart.com.br",
+      technology: "Vanilla JS",
+    },
+    {
+      title: "Oficina RogerNight",
+      url: "https://ofrgnight.github.io",
+      technology: "Nuxt",
+    },
+    {
+      title: "Banger",
+      url: "https://banger.lzart.com.br",
+      technology: "Vanilla TS",
+    },
+    {
+      title: "Dashy",
+      url: "https://dashy.lzart.com.br",
+      technology: "Angular",
+    },
+    { title: "NGMaker", url: "https://ngmaker.com.br", technology: "Angular" },
+    {
+      title: "My Test Place",
+      url: "https://yspoof.github.io",
+      technology: "Vanilla JS",
+    },
+    {
+      title: "My Resume",
+      url: "https://luis.lzart.com.br/",
+      technology: "Solid JS",
+    },
+  ],
+};
